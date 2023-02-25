a5cef2d9dfafe4aec8bc421ca0fd2a13103e5690
Wilkrest <jarirez@hotmail.com>
2023-02-24 19:50:36 -0600

    added prettier

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,52 @@
+import { Dropdown } from 'react-bootstrap';
+
 export const Navbar = () => {
-  return (
-    <div style={mainNavbar}>
-      <NavbarStart />
-      <NavbarCenter />
-      <NavbarEnd />
-    </div>
-  );
+    return (
+        <div style={mainNavbar}>
+            <NavbarStart />
+            <NavbarCenter />
+            <NavbarEnd />
+        </div>
+    );
 };
 
 const NavbarStart = () => {
-  return (
-    <div style={logoContainer}>
-      <h1 style={logo}>BGN LOGO</h1>
-    </div>
-  );
+    return (
+        <div style={logoContainer}>
+            <h1 style={logo}>BGN LOGO</h1>
+        </div>
+    );
 };
 
 const NavbarCenter = () => {
-  return <div></div>;
+    return <div></div>;
 };
 
 const NavbarEnd = () => {
-  return <div>end</div>;
+    return (
+        <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Dropdown Button
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    );
 };
 
 const mainNavbar = {
-  backgroundColor: "aquamarine",
-  display: "flex",
-  height: "fit-content",
-  justifyContent: "space-between",
+    backgroundColor: 'aquamarine',
+    display: 'flex',
+    height: 'fit-content',
+    justifyContent: 'space-between',
 };
 
 const logoContainer = {
-  marginLeft: "10px",
+    marginLeft: '10px',
 };
 
 const logo = {};
